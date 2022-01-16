66679f27fcdfec4c385f528ecedded77d32480f3
Marius Lange <marius.lange1997@gmail.com>
2022-01-16 15:56:49 +0100

    added sep kafka consumer

--- kafka-consumer/src/main/java/de/marius/kafkaconsumer/OwnConsumerPlainKafka.java
+++ kafka-consumer/src/main/java/de/marius/kafkaconsumer/OwnConsumerPlainKafka.java
@@ -14,6 +14,7 @@ import org.springframework.stereotype.Component;
 
 import java.time.Duration;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.Properties;
 
 @Slf4j
@@ -44,13 +45,22 @@ public class OwnConsumerPlainKafka {
         //Subscribing
         consumer.subscribe(Arrays.asList(topic));
         //polling
-        while (true) {
-            ConsumerRecords<String, String> records = consumer.poll(Duration.ofMillis(100));
-            log.info("polling...");
-            for (ConsumerRecord<String, String> record : records) {
-                log.info("Key: " + record.key() + ", Value:" + record.value());
-                log.info("Partition:" + record.partition() + ",Offset:" + record.offset());
-            }
-        }
+        consumer.poll(Duration.ofMillis(100));
+        consumer.seekToBeginning(Collections.emptySet());
+        for (ConsumerRecord record : consumer.poll(Duration.ofMillis(100)).records(topic)) {
+        log.info("Key: " + record.key() + ", Value:" + record.value());
+        log.info("Partition:" + record.partition() + ",Offset:" + record.offset());
+    }
+
+
+//        while (true) {
+//            ConsumerRecords<String, String> records = consumer.poll(Duration.ofMillis(100));
+//            log.info("polling...");
+//            for (ConsumerRecord<String, String> record : records) {
+//                log.info("Key: " + record.key() + ", Value:" + record.value());
+//                log.info("Partition:" + record.partition() + ",Offset:" + record.offset());
+//
+//            }
+//        }
     }
 }
